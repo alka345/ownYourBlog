e2be9c590a9ca0e101e33e88240e1547b7e98c5a
alka345 <alkaraojnv@gmail.com>
2024-04-27 11:59:18 +0530

    appwrite conf

--- BlogWebsite/src/App.jsx
+++ BlogWebsite/src/App.jsx
@@ -1,8 +1,12 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  // console.log(import.meta.env.VITE_APPWRITE_URL);
+console.log(import.meta.env.VITE_APPWRITE_URL );
+console.log(import.meta.env.VITE_APPWRITE_PROJECT_ID );
+console.log(import.meta.env.VITE_APPWRITE_DATABASE_ID );
+console.log(import.meta.env.VITE_APPWRITE_COLLECTION_ID );
+console.log(import.meta.env.VITE_APPWRITE_BUCKET_ID );
 
   return (
   <div>Blog</div> 
